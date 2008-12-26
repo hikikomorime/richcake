a729bca61326b999b395e8769c1f031c43a68f70
mfandrade <mfandrade@b691fc66-c235-11dd-b31d-17274aa020d1>
2008-12-26 04:35:47 +0000

    Remoção da árvore do projeto para atualização para versão estável 1.2.

--- trunk/cake_1.2.0.7692-rc3/app/controllers/richdemo_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-class RichdemoController extends AppController {
-    var $name   = "richdemo";
-    var $helper = array("Rich");
-
-    function index() {
-    }
-}
-?>
--- trunk/cake_1.2.0.7692-rc3/app/models/richdemo.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Richdemo extends AppModel {
-    var $name       = "richdemo";
-    var $useTable   = false;
-}
-?>
